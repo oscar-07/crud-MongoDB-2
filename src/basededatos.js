c876a6014656b691f37517c130ac5a3925feb0f2
oscar-07 <60765149+oscar-07@users.noreply.github.com>
2021-10-19 17:14:34 -0500

    Update basededatos.js

--- src/basededatos.js
+++ src/basededatos.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://4rub1k:Eljuegodelcalamar@principal.s1h7n.mongodb.net/Notasytitulos?retryWrites=true&w=majority',{
+mongoose.connect('Liga mongo',{
 
     useUnifiedTopology: true 
 })
